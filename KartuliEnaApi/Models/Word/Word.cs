50c22b5ab76f8400a34d689f56734b72c777e926
lexxA87 <alex.o.nikonov@gmail.com>
2023-01-19 19:06:55 +0400

    Added model for Word

---
+++ KartuliEnaApi/Models/Word/Word.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace KartuliEnaApi.Models
+{
+    public class Word
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Ka { get; set; } = null!;
+        public string Ru { get; set; } = null!;
+        public string En { get; set; } = null!;
+        public string[] Examples { get; set; }
+        public string[] Tags { get; set; }
+        public string Role { get; set; } = null!;
+    }
+}
