e02ebba97909d026d1186e01b3d7e82ffc92f8de
lexxA87 <alex.o.nikonov@gmail.com>
2023-01-19 19:22:08 +0400

    Added settings for database

---
+++ KartuliEnaApi/Models/WordDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace KartuliEnaApi.Models
+{
+    public class WordDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string WordsCollectionName { get; set; } = null!;
+    }
+}
