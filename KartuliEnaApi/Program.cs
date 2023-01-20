07dea354651fdd409ed5e2f8f96d90e219b64ca7
lexxA87 <alex.o.nikonov@gmail.com>
2023-01-21 00:39:48 +0400

    Changed program.cs and lounchSettings I think CoreAdmin doesn't work with MongoDb :(

--- KartuliEnaApi/Program.cs
+++ KartuliEnaApi/Program.cs
@@ -26,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
